bcc9810107752544f2341d93b2272a80b02212d6
Aristov Konstantin <kostya.74.74@gmail.com>
2022-06-19 02:29:12 +0500

    Сведения о возможных полях

--- src/FieldsInfo.jsx
+++ src/FieldsInfo.jsx
@@ -4,7 +4,9 @@ export default function FieldsInfo() {
   const [fieldsInfo, setFieldsInfo] = React.useState('');
 
   React.useEffect(() => {
-    fetch('/api/fieldsInfo', { method: 'GET' }).then((resp) => setFieldsInfo(resp.text()));
+    fetch('/api/fieldsInfo', { method: 'GET' })
+      .then((resp) => resp.text()
+        .then((fieldInfo) => setFieldsInfo(fieldInfo)));
   }, []);
 
   return (
