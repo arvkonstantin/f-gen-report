be1dbe2731f1dfadb8b3bf126e368b7f24966570
Aristov Konstantin <kostya.74.74@gmail.com>
2022-06-18 15:52:41 +0500

    Отправка файла на бэк

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const onFileSelect = (e) => {
+  const formData = new FormData();
+  formData.append('zip', e.target.files[0]);
+  // noinspection JSIgnoredPromiseFromCall
+  fetch('/api/upload', { method: 'POST', body: formData });
+};
+
 root.render(
-  <React.StrictMode />,
+  <React.StrictMode>
+    <input type="file" onChange={onFileSelect} />
+  </React.StrictMode>,
 );
